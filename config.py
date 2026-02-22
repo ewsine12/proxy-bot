c75abf43c81c4e46ebaba619f7f30eaa6de6b8ef
ewsine12 <ewsine11@gmail.com>
2026-02-22 03:23:00 +0300

    Add configuration for bot and API keys

---
+++ config.py
@@ -0,0 +1,6 @@
+import os
+
+BOT_TOKEN = os.getenv("8441199400:AAEv4bCcZQGF4RnyG727ag-HUynhPsDnGfs")          # Telegram Bot Token
+PROXY_API_KEY = os.getenv("c179d173cc926702eb4378c8f9104ee7")  # Proxy-Seller API Key
+ADMIN_ID = int(os.getenv("7149740820"))       # Admin Telegram ID
+API_URL = "https://proxy-seller.com/personal/api/v1/{c179d173cc926702eb4378c8f9104ee7}/proxy/list/{type"    # Proxy-Seller API Base URL
